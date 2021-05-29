ddcc630856abe1e412790ab8c61559313e66e8ec
Shan_Chen <tarasuzy00@gmail.com>
2021-05-29 13:08:32 +0800

    First commit

---
+++ SnakeGame/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SnakeGame
+//
+//  Created by Shan OvO on 2021/5/29.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
